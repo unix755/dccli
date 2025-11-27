dd3a174b703f36cf0457c43e670c3492c19fa719
unix755 <10346089+unix755@users.noreply.github.com>
2025-11-27 17:48:07 +0800

    up

---
+++ .github/workflows/Build + Release.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# building and compressing
+git clone https://github.com/unix755/gobd
+go build -C gobd -o "../gobd_runner"
+chmod +x ./gobd_runner
+./gobd_runner -os windows -d bin -no_debug -no_cgo
+
+# delete all releases and their tags
+gh release list | awk -F '\t' '{print $3}' | while read -r line; do gh release delete --yes --cleanup-tag "$line"; done
+
+# create release
+if [ "${{ inputs.version }}" != "" ]; then
+  tag="${{ inputs.version }}"
+else
+  tag="$(go run . -v)"
+fi
+gh release create "$tag" --generate-notes "bin/*"
